d6eeae91c5de04dce72d65fd1b5601d04c2d5446
foobar <foo@bar.net>
2018-12-20 13:31:44 +0100

    [Editor] Top down camera override; road point set test

--- MotorwayEditor/App.cpp
+++ MotorwayEditor/App.cpp
@@ -110,6 +110,8 @@
 #include "EditorInterface.h"
 
 #include <Framework/TerrainEditor.h>
+#include <Framework/RoadEditor.h>
+
 #include <Core/Allocators/AllocationHelpers.h>
 #include <Core/Allocators/GrowingStackAllocator.h>
 #include <Core/Allocators/LinearAllocator.h>
@@ -534,9 +536,11 @@ void InitializeSubsystems()
                 // Restore current editor input context
                 if ( *panelId == 2 ) {
                     g_InputMapper->pushContext( FLAN_STRING_HASH( "TerrainEditor" ) );
+                } else if ( *panelId == 3 ) {
+                    g_InputMapper->pushContext( FLAN_STRING_HASH( "RoadEditor" ) );
                 }
             } else {
-                if ( *panelId == 2 ) {
+                if ( *panelId == 2 || *panelId == 3 ) {
                     g_InputMapper->popContext();
                 }
 
@@ -556,7 +560,22 @@ void InitializeSubsystems()
 
         ImGuiIO& io = ImGui::GetIO();
         if ( !io.WantCaptureMouse ) {
-            if ( input.States.find( FLAN_STRING_HASH( "EditTerrainHeight" ) ) != input.States.end() ) {
+            if ( input.Actions.find( FLAN_STRING_HASH( "AddPointToRoad" ) ) != input.Actions.end() ) {
+                if ( PickedNode != nullptr ) {
+                    g_EditedTerrainSceneEditor = ( TerrainSceneNode* )PickedNode;
+
+                    const auto& cameraData = mainCamera->GetData();
+                    Ray rayObj = GenerateMousePickingRay( io, cameraData );
+
+                    flan::framework::UpdateHeightfieldMouseCircle( rayObj, g_EditedTerrainSceneEditor->instance.terrainAsset );
+
+                    auto cmdList = g_EditorCmdListPool->allocateCmdList( g_RenderDevice );
+                    cmdList->beginCommandList( g_RenderDevice );
+                    flan::framework::AddPointToRoad( rayObj, g_EditedTerrainSceneEditor->instance.terrainAsset, cmdList );
+                    cmdList->endCommandList( g_RenderDevice );
+                    cmdList->playbackCommandList( g_RenderDevice );
+                }
+            } else if ( input.States.find( FLAN_STRING_HASH( "EditTerrainHeight" ) ) != input.States.end() ) {
                 if ( PickedNode != nullptr ) {
                     g_EditedTerrainSceneEditor = ( TerrainSceneNode* )PickedNode;
                     g_IsEditingTerrain = true;
