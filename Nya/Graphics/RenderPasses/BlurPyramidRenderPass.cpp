d8a77db7a613580beea0a9b063e1b514ab02fbb3
foobar <foo@bar.net>
2019-02-15 17:08:01 +0100

    [Bloom] Single CS idea

--- Nya/Graphics/RenderPasses/BlurPyramidRenderPass.cpp
+++ Nya/Graphics/RenderPasses/BlurPyramidRenderPass.cpp
@@ -7,26 +7,21 @@
 #include <Rendering/ImageFormat.h>
 #include <Rendering/CommandList.h>
 
-static PipelineState*  g_PipelineStateObjectWeightedKaris = nullptr;
 static PipelineState*  g_PipelineStateObject = nullptr;
 
 void LoadCachedResourcesBP( RenderDevice* renderDevice, ShaderCache* shaderCache )
 {
     PipelineStateDesc psoDesc = {};
-    psoDesc.computeShader = shaderCache->getOrUploadStage( "PostFX/DownsampleWeighted+NYA_USE_KARIS_AVERAGE", SHADER_STAGE_COMPUTE );
-    g_PipelineStateObjectWeightedKaris = renderDevice->createPipelineState( psoDesc );
-
     psoDesc.computeShader = shaderCache->getOrUploadStage( "PostFX/DownsampleWeighted", SHADER_STAGE_COMPUTE );
     g_PipelineStateObject = renderDevice->createPipelineState( psoDesc );
 }
 
 void FreeCachedResourcesBP( RenderDevice* renderDevice )
 {
-    renderDevice->destroyPipelineState( g_PipelineStateObjectWeightedKaris );
     renderDevice->destroyPipelineState( g_PipelineStateObject );
 }
 
-ResHandle_t AddDownsampleMipRenderPass( RenderPipeline* renderPipeline, ResHandle_t input, const uint32_t inputWidth, const uint32_t inputHeight, const uint32_t downsampleFactor = 2u, const bool useKarisAverage = false )
+ResHandle_t AddDownsampleMipRenderPass( RenderPipeline* renderPipeline, ResHandle_t input, const uint32_t inputWidth, const uint32_t inputHeight, const uint32_t downsampleFactor = 2u )
 {
     struct PassData
     {
@@ -39,17 +34,17 @@ ResHandle_t AddDownsampleMipRenderPass( RenderPipeline* renderPipeline, ResHandl
     PassData& passData = renderPipeline->addRenderPass<PassData>(
         "Downsample Weighted Pass",
         [&]( RenderPipelineBuilder& renderPipelineBuilder, PassData& passData ) {
-            renderPipelineBuilder.setUncullablePass();
+            //renderPipelineBuilder.setUncullablePass(); // test for debug >> remove it later!
 
-            passData.input = ( useKarisAverage ) ? renderPipelineBuilder.readRenderTarget( input ) : renderPipelineBuilder.readBuffer( input );
+            passData.input = renderPipelineBuilder.readRenderTarget( input );
             
-            float downsample = 1.0f / downsampleFactor;
+            const float downsample = 1.0f / downsampleFactor;
 
             BufferDesc bufferDesc = {};
             bufferDesc.type = BufferDesc::UNORDERED_ACCESS_VIEW_TEXTURE_2D;
             bufferDesc.viewFormat = eImageFormat::IMAGE_FORMAT_R11G11B10_FLOAT;
-            bufferDesc.width = static_cast<uint32_t>( inputWidth * downsample );
-            bufferDesc.height = static_cast<uint32_t>( inputHeight * downsample );
+            bufferDesc.width = static_cast< uint32_t >( inputWidth * downsample );
+            bufferDesc.height = static_cast< uint32_t >( inputHeight * downsample );
             bufferDesc.depth = 1;
             bufferDesc.mipCount = 1;
 
@@ -74,25 +69,16 @@ ResHandle_t AddDownsampleMipRenderPass( RenderPipeline* renderPipeline, ResHandl
             ResourceList& resourceList = renderDevice->allocateResourceList( resListDesc );
             cmdList->bindResourceList( &resourceList );
 
+            RenderTarget* inputTarget = renderPipelineResources.getRenderTarget( passData.input );
+
             // RenderPass
             RenderPassDesc passDesc = {};
-            if ( useKarisAverage ) {
-                RenderTarget* inputTarget = renderPipelineResources.getRenderTarget( passData.input );
-                
-                passDesc.attachements[0] = { inputTarget, SHADER_STAGE_COMPUTE, RenderPassDesc::READ, RenderPassDesc::DONT_CARE };
-            } else {
-                Buffer* inputBuffer = renderPipelineResources.getBuffer( passData.input );
-
-                passDesc.attachements[0].buffer = inputBuffer;
-                passDesc.attachements[0].stageBind = SHADER_STAGE_COMPUTE;
-                passDesc.attachements[0].bindMode = RenderPassDesc::READ;
-                passDesc.attachements[0].targetState = RenderPassDesc::IS_UAV_TEXTURE;
-            }
-
+            passDesc.attachements[0] = { inputTarget, SHADER_STAGE_COMPUTE, RenderPassDesc::READ, RenderPassDesc::DONT_CARE };
+           
             RenderPass* renderPass = renderDevice->createRenderPass( passDesc );
             cmdList->useRenderPass( renderPass );
 
-            cmdList->bindPipelineState( useKarisAverage ? g_PipelineStateObjectWeightedKaris : g_PipelineStateObject );
+            cmdList->bindPipelineState( g_PipelineStateObject );
 
             cmdList->dispatchCompute( inputWidth / 16u, inputHeight / 16u, 1u );
 
@@ -106,11 +92,11 @@ ResHandle_t AddDownsampleMipRenderPass( RenderPipeline* renderPipeline, ResHandl
 ResHandle_t AddBlurPyramidRenderPass( RenderPipeline* renderPipeline, ResHandle_t input, uint32_t inputWidth, uint32_t inputHeight )
 {
     // Downsample the mipchain
-    ResHandle_t mip0RenderTarget = AddDownsampleMipRenderPass( renderPipeline, input, inputWidth, inputHeight, 2, true );
-    ResHandle_t mip1RenderTarget = AddDownsampleMipRenderPass( renderPipeline, mip0RenderTarget, inputWidth, inputHeight );
+    ResHandle_t mip0RenderTarget = AddDownsampleMipRenderPass( renderPipeline, input, inputWidth, inputHeight, 2 );
+   /* ResHandle_t mip1RenderTarget = AddDownsampleMipRenderPass( renderPipeline, mip0RenderTarget, inputWidth, inputHeight );
     ResHandle_t mip2RenderTarget = AddDownsampleMipRenderPass( renderPipeline, mip1RenderTarget, inputWidth, inputHeight );
     ResHandle_t mip3RenderTarget = AddDownsampleMipRenderPass( renderPipeline, mip2RenderTarget, inputWidth, inputHeight );
     ResHandle_t mip4RenderTarget = AddDownsampleMipRenderPass( renderPipeline, mip3RenderTarget, inputWidth, inputHeight );
-
+*/
     return mip0RenderTarget;
 }
