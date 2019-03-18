97b2b368a0c624bf8d5e47afe46a93500778b2e9
foobar <foo@bar.net>
2019-03-18 15:43:21 +0100

    [Shader] Light culling shader rewrite

--- Nya/Shaders/Lighting/LightCulling.hlsl
+++ Nya/Shaders/Lighting/LightCulling.hlsl
@@ -3,36 +3,39 @@
 
 static const int NUM_THREADS_X = 16;
 static const int NUM_THREADS_Y = 1;
-static const int NUM_THREADS = ( NUM_THREADS_X * NUM_THREADS_Y );
+static const int NUM_THREADS_Z = 1;
+static const int NUM_THREADS = ( NUM_THREADS_X * NUM_THREADS_Y * NUM_THREADS_Z );
 
-static const int CLUSTER_X = 32;
+static const int CLUSTER_X = 16;
 static const int CLUSTER_Y = 8;
-static const int CLUSTER_Z = 32;
+static const int CLUSTER_Z = 24;
 
 static const uint CLUSTER_COUNT = ( CLUSTER_Z * CLUSTER_Y * CLUSTER_X );
 
 RWTexture3D<uint2> g_LightClusters : register( u0 );
+RWTexture1D<uint> g_ItemList : register( u1 );
 
-groupshared uint g_LightMasksPointLights[CLUSTER_Z][CLUSTER_Y][CLUSTER_X];
+groupshared uint2 g_ClusterInfos; // X: item list offset Y: current cluster PointLight/SpotLight/LightProbe Count (12/12/8 bits)
 
-[numthreads( NUM_THREADS_X, NUM_THREADS_Y, 1 )]
+groupshared uint  g_ClusterItems[MAX_POINT_LIGHT_COUNT + MAX_LOCAL_IBL_PROBE_COUNT];
+groupshared uint  g_ClusterItemListOffset;
+
+[numthreads( NUM_THREADS_X, NUM_THREADS_Y, NUM_THREADS_Z )]
 void EntryPointCS( uint3 globalIdx : SV_DispatchThreadID, uint3 localIdx : SV_GroupThreadID, uint3 groupIdx : SV_GroupID )
 {
-    uint localIdxFlattened = localIdx.x + localIdx.y * NUM_THREADS_X;
+    uint localIdxFlattened = localIdx.x + localIdx.y * NUM_THREADS_X + localIdx.z * NUM_THREADS_X * NUM_THREADS_Y;
 	
 	// Initialize LDS memory
     if ( localIdxFlattened == 0 ) {
-		for ( int z = 0; z < CLUSTER_Z; ++z ) {
-			for ( int y = 0; y < CLUSTER_Y; ++y ) {
-				for ( int x = 0; x < CLUSTER_X; ++x ) {
-					g_LightMasksPointLights[z][y][x] = 0u;
-				}
-			}
+		g_ClusterItemListOffset = 0u;
+		
+		for ( int x = 0; x < ( MAX_POINT_LIGHT_COUNT + MAX_LOCAL_IBL_PROBE_COUNT ); ++x ) {
+			g_ClusterItems[x] = 0u;
 		}
 	}
 	
 	[unroll]
-    for ( uint i = localIdxFlattened; i < MAX_POINT_LIGHT_COUNT; i += NUM_THREADS ) {
+    for ( uint i = 0; i < MAX_POINT_LIGHT_COUNT; i++ ) {
 		PointLight light = g_PointLights[i];
 		
         const float3 p = ( light.PositionAndRadius.xyz - g_SceneAABBMin );
@@ -43,7 +46,7 @@ void EntryPointCS( uint3 globalIdx : SV_DispatchThreadID, uint3 localIdx : SV_Gr
         const int px = ( int )floor( p.x * g_ClustersScale.x );
         const int py = ( int )floor( p.y * g_ClustersScale.y );
         const int pz = ( int )floor( p.z * g_ClustersScale.z );
-
+	
         // Cluster bounds for the light
         const int x0 = max( ( int )floor( pMin.x ), 0 );
         const int x1 = min( ( int )ceil( pMax.x ), CLUSTER_X );
@@ -51,119 +54,127 @@ void EntryPointCS( uint3 globalIdx : SV_DispatchThreadID, uint3 localIdx : SV_Gr
         const int y1 = min( ( int )ceil( pMax.y ), CLUSTER_Y );
         const int z0 = max( ( int )floor( pMin.z ), 0 );
         const int z1 = min( ( int )ceil( pMax.z ), CLUSTER_Z );
-
-        const float squaredRadius = light.PositionAndRadius.w * light.PositionAndRadius.w;
-        const uint mask = ( 1 << i );
-		
-		[loop]
-        for ( int z = z0; z < z1; z++ ) {
-            float dz = ( pz == z ) ? 0.0f : g_SceneAABBMin.z + ( ( pz < z ) ? z : z + 1 ) * g_ClustersInverseScale.z - light.PositionAndRadius.z;
-            dz *= dz;
-            
-			[loop]
-            for ( int y = y0; y < y1; y++ ) {
-                float dy = ( py == y ) ? 0.0f : g_SceneAABBMin.y + ( ( py < y ) ? y : y + 1 ) * g_ClustersInverseScale.y - light.PositionAndRadius.y;
-                dy *= dy;
-                dy += dz;
-				
-				[loop]
-                for ( int x = x0; x < x1; x++ ) {
-                    float dx = ( px == x ) ? 0.0f : g_SceneAABBMin.x + ( ( px < x ) ? x : x + 1 ) * g_ClustersInverseScale.x - light.PositionAndRadius.x;
-                    dx *= dx;
-                    dx += dy;
-                     
-					[branch]             			
-                    if ( dx < squaredRadius ) {
-                        g_LightMasksPointLights[z][y][x] |= mask;
-                    }
-                }
-            }
-        }
-	}
-	
-    GroupMemoryBarrierWithGroupSync();
-    
-    if ( localIdxFlattened == 0 ) {
-        // Copy Point Light Masks
-		for ( int z = 0; z < CLUSTER_Z; ++z ) {
-			for ( int y = 0; y < CLUSTER_Y; ++y ) {
-				for ( int x = 0; x < CLUSTER_X; ++x ) {
-					g_LightClusters[uint3( x, y, z )] = uint2( g_LightMasksPointLights[z][y][x], 0u );
-				}
-			}
-		}
-    }
         
-    if ( localIdxFlattened == 0 ) {
-		for ( int z = 0; z < CLUSTER_Z; ++z ) {
-			for ( int y = 0; y < CLUSTER_Y; ++y ) {
-				for ( int x = 0; x < CLUSTER_X; ++x ) {
-					g_LightMasksPointLights[z][y][x] = 0u;
-				}
-			}
+		const float squaredRadius = light.PositionAndRadius.w * light.PositionAndRadius.w;
+		
+		// Skip if light is outside current cluster bounds
+		[branch]
+		if ( localIdx.x < x0 
+			|| localIdx.x > x1 
+			|| localIdx.y < y0 
+			|| localIdx.y > y1 
+			|| localIdx.z < z0 
+			|| localIdx.z > z1 ) {
+			continue;
 		}
-    }
+		
+		float dz = ( pz == localIdx.z ) 
+			? 0.0f 
+			: g_SceneAABBMin.z + ( ( pz < localIdx.z ) ? localIdx.z : localIdx.z + 1 ) * g_ClustersInverseScale.z - light.PositionAndRadius.z;
+		dz *= dz;
+		
+		float dy = ( py == localIdx.y ) 
+			? 0.0f 
+			: g_SceneAABBMin.y + ( ( py < localIdx.y ) ? localIdx.y : localIdx.y + 1 ) * g_ClustersInverseScale.y - light.PositionAndRadius.y;
+		dy *= dy;
+		dy += dz;
+		
+		float dx = ( px == localIdx.x ) 
+			? 0.0f 
+			: g_SceneAABBMin.x + ( ( px < localIdx.x ) ? localIdx.x : localIdx.x + 1 ) * g_ClustersInverseScale.x - light.PositionAndRadius.x;
+		dx *= dx;
+		dx += dy;
+	
+		[branch]             			
+		if ( dx < squaredRadius ) {
+			g_ClusterItems[g_ClusterItemListOffset] = i;
+			InterlockedAdd( g_ClusterItemListOffset, 1u );
+		}
+	}
     
-    [unroll]
-    for ( uint j = localIdxFlattened; j < MAX_LOCAL_IBL_PROBE_COUNT; j += NUM_THREADS ) {
-		IBLProbe probe = g_IBLProbes[j + 1u];
-		
-        const float3 p = ( probe.PositionAndRadius.xyz - g_SceneAABBMin );
-        const float3 pMin = ( p - probe.PositionAndRadius.w ) * g_ClustersScale;
-        const float3 pMax = ( p + probe.PositionAndRadius.w ) * g_ClustersScale;
-
-        // Cluster for the center of the probe
-        const int px = ( int )floor( p.x * g_ClustersScale.x );
-        const int py = ( int )floor( p.y * g_ClustersScale.y );
-        const int pz = ( int )floor( p.z * g_ClustersScale.z );
-
-        // Cluster bounds for the probe
-        const int x0 = max( ( int )floor( pMin.x ), 0 );
-        const int x1 = min( ( int )ceil( pMax.x ), CLUSTER_X );
-        const int y0 = max( ( int )floor( pMin.y ), 0 );
-        const int y1 = min( ( int )ceil( pMax.y ), CLUSTER_Y );
-        const int z0 = max( ( int )floor( pMin.z ), 0 );
-        const int z1 = min( ( int )ceil( pMax.z ), CLUSTER_Z );
-
-        const float squaredRadius = probe.PositionAndRadius.w * probe.PositionAndRadius.w;
-        const uint mask = ( 1 << j );
-		
-		[loop]
-        for ( int z = z0; z < z1; z++ ) {
-            float dz = ( pz == z ) ? 0.0f : g_SceneAABBMin.z + ( ( pz < z ) ? z : z + 1 ) * g_ClustersInverseScale.z - probe.PositionAndRadius.z;
-            dz *= dz;
-            
-			[loop]
-            for ( int y = y0; y < y1; y++ ) {
-                float dy = ( py == y ) ? 0.0f : g_SceneAABBMin.y + ( ( py < y ) ? y : y + 1 ) * g_ClustersInverseScale.y - probe.PositionAndRadius.y;
-                dy *= dy;
-                dy += dz;
-				
-				[loop]
-                for ( int x = x0; x < x1; x++ ) {
-                    float dx = ( px == x ) ? 0.0f : g_SceneAABBMin.x + ( ( px < x ) ? x : x + 1 ) * g_ClustersInverseScale.x - probe.PositionAndRadius.x;
-                    dx *= dx;
-                    dx += dy;
-                     
-					[branch]             			
-                    if ( dx < squaredRadius ) {
-                        g_LightMasksPointLights[z][y][x] |= mask;
-                    }
-                }
-            }
-        }
-	}
-	
     GroupMemoryBarrierWithGroupSync();
+    
+    // if ( localIdxFlattened == 0 ) {
+        // // Copy Point Light Masks
+		// for ( int z = 0; z < CLUSTER_Z; ++z ) {
+			// for ( int y = 0; y < CLUSTER_Y; ++y ) {
+				// for ( int x = 0; x < CLUSTER_X; ++x ) {
+					// g_LightClusters[uint3( x, y, z )] = uint2( g_LightMasksPointLights[z][y][x], 0u );
+				// }
+			// }
+		// }
+    // }
+        
+    // if ( localIdxFlattened == 0 ) {
+		// for ( int z = 0; z < CLUSTER_Z; ++z ) {
+			// for ( int y = 0; y < CLUSTER_Y; ++y ) {
+				// for ( int x = 0; x < CLUSTER_X; ++x ) {
+					// g_LightMasksPointLights[z][y][x] = 0u;
+				// }
+			// }
+		// }
+    // }
+    
+    // [unroll]
+    // for ( uint j = localIdxFlattened; j < MAX_LOCAL_IBL_PROBE_COUNT; j += NUM_THREADS ) {
+		// IBLProbe probe = g_IBLProbes[j + 1u];
+		
+        // const float3 p = ( probe.PositionAndRadius.xyz - g_SceneAABBMin );
+        // const float3 pMin = ( p - probe.PositionAndRadius.w ) * g_ClustersScale;
+        // const float3 pMax = ( p + probe.PositionAndRadius.w ) * g_ClustersScale;
+
+        // // Cluster for the center of the probe
+        // const int px = ( int )floor( p.x * g_ClustersScale.x );
+        // const int py = ( int )floor( p.y * g_ClustersScale.y );
+        // const int pz = ( int )floor( p.z * g_ClustersScale.z );
+
+        // // Cluster bounds for the probe
+        // const int x0 = max( ( int )floor( pMin.x ), 0 );
+        // const int x1 = min( ( int )ceil( pMax.x ), CLUSTER_X );
+        // const int y0 = max( ( int )floor( pMin.y ), 0 );
+        // const int y1 = min( ( int )ceil( pMax.y ), CLUSTER_Y );
+        // const int z0 = max( ( int )floor( pMin.z ), 0 );
+        // const int z1 = min( ( int )ceil( pMax.z ), CLUSTER_Z );
+
+        // const float squaredRadius = probe.PositionAndRadius.w * probe.PositionAndRadius.w;
+        // const uint mask = ( 1 << j );
+		
+		// [loop]
+        // for ( int z = z0; z < z1; z++ ) {
+            // float dz = ( pz == z ) ? 0.0f : g_SceneAABBMin.z + ( ( pz < z ) ? z : z + 1 ) * g_ClustersInverseScale.z - probe.PositionAndRadius.z;
+            // dz *= dz;
+            
+			// [loop]
+            // for ( int y = y0; y < y1; y++ ) {
+                // float dy = ( py == y ) ? 0.0f : g_SceneAABBMin.y + ( ( py < y ) ? y : y + 1 ) * g_ClustersInverseScale.y - probe.PositionAndRadius.y;
+                // dy *= dy;
+                // dy += dz;
+				
+				// [loop]
+                // for ( int x = x0; x < x1; x++ ) {
+                    // float dx = ( px == x ) ? 0.0f : g_SceneAABBMin.x + ( ( px < x ) ? x : x + 1 ) * g_ClustersInverseScale.x - probe.PositionAndRadius.x;
+                    // dx *= dx;
+                    // dx += dy;
+                     
+					// [branch]             			
+                    // if ( dx < squaredRadius ) {
+                        // g_LightMasksPointLights[z][y][x] |= mask;
+                    // }
+                // }
+            // }
+        // }
+	// }
 	
-    if ( localIdxFlattened == 0 ) {
-        // Copy IBL Probe Masks
-		for ( int z = 0; z < CLUSTER_Z; ++z ) {
-			for ( int y = 0; y < CLUSTER_Y; ++y ) {
-				for ( int x = 0; x < CLUSTER_X; ++x ) {
-					g_LightClusters[uint3( x, y, z )] = uint2( g_LightClusters[uint3( x, y, z )].r, g_LightMasksPointLights[z][y][x] );
-				}
-			}
-		}
-	}
+    // GroupMemoryBarrierWithGroupSync();
+	
+    // if ( localIdxFlattened == 0 ) {
+        // // Copy IBL Probe Masks
+		// for ( int z = 0; z < CLUSTER_Z; ++z ) {
+			// for ( int y = 0; y < CLUSTER_Y; ++y ) {
+				// for ( int x = 0; x < CLUSTER_X; ++x ) {
+					// g_LightClusters[uint3( x, y, z )] = uint2( g_LightClusters[uint3( x, y, z )].r, g_LightMasksPointLights[z][y][x] );
+				// }
+			// }
+		// }
+	// }
 }
